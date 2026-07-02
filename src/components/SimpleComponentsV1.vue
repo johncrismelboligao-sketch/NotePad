58188a43044fd264cd178772159840e6f59ce010
johncrismelboligao-sketch <johncrismelboligao@gmail.com>
2026-07-02 10:57:39 +0800

    commit

---
+++ src/components/SimpleComponentsV1.vue
@@ -0,0 +1,11 @@
+<script setup>
+import SimpleComponents from './components/SimpleComponetsV1.vue'
+</script>
+
+<template>
+    <SimpleComponents>
+        <div>
+            <h1>Joga na</h1>
+        </div>
+    </SimpleComponents>
+</template>
